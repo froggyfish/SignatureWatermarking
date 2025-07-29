39982e119dd85b6b2dc3d53ab770e13f1805e210
Bryce Crouse <brycecrouse@gmail.com>
2025-07-29 03:11:43 -0600

    bugflix

--- encode.py
+++ encode.py
@@ -140,9 +140,9 @@ for i in tqdm(range(test_num)):
             shape = (1, 4, 64, 64)
             init_latents, _, _ = tr_get_noise(shape, from_file=tr_key, keys_path='keys/')
         elif method == 'sig':
-            scheme = SignatureScheme()
-            # 2. Generate a key pair for the signer.
-            signer_private_key, signer_public_key = SignatureScheme.generate_keys(729)
+            # scheme = SignatureScheme()
+            # # 2. Generate a key pair for the signer.
+            # signer_private_key, signer_public_key = SignatureScheme.generate_keys(729)
 
             message = b"hi"
             # Load the codeword tensor from the saved file
